fee40f732c055d4989efcd80bbddcfb44a4d1c63
gbsk12345 <104025468+gbsk12345@users.noreply.github.com>
2023-06-22 21:09:59 +0530

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import people from "./data.js";
+
+import { FaBeer } from "react-icons/fa";
+const App = () => {
+  return ()
+  <div>
+    <h2>Reviews Starter</h2>;
+    <FaBeer className="beer" />
+  </div>);
+};
+export default App;
